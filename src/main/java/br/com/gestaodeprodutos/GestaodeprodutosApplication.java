5b6354dc7213e2dfc50225a8837fc011d4221ebe
Guilherme <guilherme.antonio@digicade.com.br>
2021-07-29 15:35:49 -0300

    strart projeto gestão de produtos

---
+++ src/main/java/br/com/gestaodeprodutos/GestaodeprodutosApplication.java
@@ -0,0 +1,13 @@
+package br.com.gestaodeprodutos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaodeprodutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaodeprodutosApplication.class, args);
+	}
+
+}
